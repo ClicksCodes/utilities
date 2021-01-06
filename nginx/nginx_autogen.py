9e027080d0331ade8680d4476e9569fcde5e3dba
Nathan <nathan@clicksminuteper.net>
2021-01-06 18:51:11 +0000

    Update nginx_autogen.py

--- nginx/nginx_autogen.py
+++ nginx/nginx_autogen.py
@@ -21,6 +21,7 @@ server {{
         proxy_pass http://{host}:{port};
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header Connection "upgrade";
     }}
 
@@ -45,6 +46,7 @@ server {{
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "upgrade";
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     }}
 
     listen [::]:80;
