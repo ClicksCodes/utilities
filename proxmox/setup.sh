21490ea03d6a89cd2c2388f75fa3a195dfcafb36
Nathan <nathan@clicksminuteper.net>
2021-01-20 15:57:00 +0000

    Update setup.sh

--- proxmox/setup.sh
+++ proxmox/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the container types that we want
-pveam download local debian-10-standard_10.5-1_amd64.tar.gz
+pveam download local debian-10-standard_10.*_amd64.tar.gz
 
 # Remove the proxmox "no valid subscription" message
 sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
